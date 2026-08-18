51b0484a5b90cc5f3afbd8f21d2c28e417bfc1a2
Caleb Josue Ruiz Torres <calebjosue-dev@outlook.com>
2026-08-18 02:23:44 -0600

    Emosongi is now a single-page application, related cleaning

--- emosongi/urls.py
+++ emosongi/urls.py
@@ -6,6 +6,4 @@ app_name = "emosongi"
 urlpatterns = [
     path("", views.index, name="index"),
     path("recommend/", views.recommend, name="recommend"),
-    path("unauthorized/", views.unauthorized, name="unauthorized"),
-    path("exceeded/", views.exceeded, name="exceeded"),
 ]
